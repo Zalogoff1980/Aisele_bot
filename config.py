ead9790b74a305b5e9b8cc6c36205aa58fed1e06
Zalogoff1980 <72643171+Zalogoff1980@users.noreply.github.com>
2026-08-22 14:55:59 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,25 @@
+import os
+
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+
+TELEGRAM_TOKEN = os.getenv(
+    "TELEGRAM_TOKEN"
+)
+
+OPENAI_API_KEY = os.getenv(
+    "OPENAI_API_KEY"
+)
+
+AI_MODEL = os.getenv(
+    "AI_MODEL",
+    "gpt-5.6"
+)
+
+MEMORY_MODEL = os.getenv(
+    "MEMORY_MODEL",
+    "gpt-5.6"
+)
